b335e6fd580458dcaa6d5d26f74a6cbe5d4759f5
Liam Brooke <24947193+ItsLimaBean@users.noreply.github.com>
2022-03-22 14:36:11 -0700

    Added getStudentsAverage function

--- src/app.js
+++ src/app.js
@@ -1,6 +1,33 @@
-'use strict';
+'use strict'
 
 function selectSidebar() {
     let sideNav = document.querySelector("#side-nav")
     console.log(sideNav)
 }
+
+function getStudentsAverage(students) {
+    const studentsScholarship = []
+    
+    for (let student of students) {
+        let gradesTotal = 0
+        for (let grade of student.grades) {
+            gradesTotal += grade
+        }
+    
+        let average = gradesTotal / student.grades.length
+        if (average > 80.0) {
+            studentsScholarship.push({ name: student.name, gradeAvg: average })
+        }
+    }
+
+    return studentsScholarship
+}
+
+const students = [
+    { name: "Tasha", grades: [ 80, 67, 75, 62 ] },
+    { name: "Tuvok", grades: [ 90, 88, 91, 92 ] },
+    { name: "Dax", grades: [ 88, 90, 89, 85 ] },
+    { name: "Neelix", grades: [ 60, 54, 48, 50 ] }
+]
+
+console.log(getStudentsAverage(students))
